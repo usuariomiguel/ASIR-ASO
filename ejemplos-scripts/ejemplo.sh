f60f092b7547e92b3bd087c4908e11aa8193b7b0
usuariomiguel <miguel@gmail.com>
2021-12-14 12:36:57 +0100

    ejemplos

--- ejemplos-scripts/ejemplo.sh
+++ ejemplos-scripts/ejemplo.sh
@@ -26,13 +26,14 @@ Existen 3 usuarios
 #!/usr/bin/env bash
 clear
 OIFS=$IFS
-IFS=:
-#cambia el separador por defecto "space" por ":"
+IFS=#
+#cambia el separador por defecto "space" por "#"
 echo -e "NOMBRE\t\tAPELLIDO\t\tTELEFONO\t\tCORREO\t\tCUMPLEAÑOS"
 while read nom ape tel email fn
 ##separa los campos por el espacio de /etc/passwd por las variables 1nom 2ape 3tel 4email 5fn
 do
     echo -e "$nom\t\t$ape\t\t$tel\t\t$email\t\t$fn"
 done < /scripts/agenda.txt
+#La ruta del fichero con los datos separador por #
 IFS=$OIFS
 
