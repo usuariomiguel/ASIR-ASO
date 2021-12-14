57f312aa976dd4e8c819f42b3c4dfbcdb729e978
usuariomiguel <miguel@gmail.com>
2021-12-14 12:15:42 +0100

    ejemplo

--- ejemplo.sh
+++ ejemplo.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 clear
+OIFS=$IFS
+IFS=:
+#cambia el separador por defecto "space" por ":"
 cont=0
-
 while read usuario pass nid resto 
+##separa los campos por el espacio de /etc/passwd por las variables 1usuario 2pass 3nid y 4resto
 do
-    echo $nid
+    echo $pass
     cont=`expr $cont + 1`
 done < /etc/passwd
 echo Existen $cont usuarios
+IFS=$OIFS
+#volvemos al separador "space"
