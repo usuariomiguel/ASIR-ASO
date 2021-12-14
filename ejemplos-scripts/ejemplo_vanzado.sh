14bae27dcd0787109f450eb19cd4e931ea7cd508
MiguelAngel <92717152+usuariomiguel@users.noreply.github.com>
2021-12-14 13:05:27 +0100

    Update ejemplo_vanzado.sh

--- ejemplos-scripts/ejemplo_vanzado.sh
+++ ejemplos-scripts/ejemplo_vanzado.sh
@@ -6,3 +6,9 @@ yad --file --title="Fichero" --width=400 --height=400 --center
 yad --list --title="Lista" --width=400 --height=400 --center
 
 yad --calendar --title="Calendario" --width=400 --height=400 --center
+
+#!/usr/bin/env bash
+yad --list --title="Gestion Agenda" --width=300 --height=250 \
+--center \
+--column=A \
+"ALTAS" "BAJAS" "CONSULTAS" "LISTADOS" "MODIFICACIONES" "SALIR"
