64540f3829c80642fe5efaa47e0950f507b26a2c
usuariomiguel <miguel@gmail.com>
2021-12-14 13:05:02 +0100

    yad

--- ejemplos-scripts/ejemplo_vanzado.sh
+++ ejemplos-scripts/ejemplo_vanzado.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-yad --color --title="Colorines" --width=400 --height=400 \ 
---center
+yad --color --title="Colorines" --width=400 --height=400 --center
+
+yad --file --title="Fichero" --width=400 --height=400 --center
+
+yad --list --title="Lista" --width=400 --height=400 --center
+
+yad --calendar --title="Calendario" --width=400 --height=400 --center
